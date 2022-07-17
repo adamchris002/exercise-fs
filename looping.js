205835d3563158e27c5b103fd72af2acd6e231e6
Adam Christoper <adamchris002@gmail.com>
2022-07-17 15:33:40 +0700

    ini file latihan looping

---
+++ looping.js
@@ -0,0 +1,89 @@
+// for (var i = 0; i < 10; i++) {
+//     console.log(i + 1)
+// }
+
+// var i = 0
+// while (i != 10) {
+//     console.log(i + 1)
+//     i++
+// }
+
+// var str = "nasi kotak"
+
+// for (var i = 0; i < str.length; i++) {
+//     console.log(i + 1)
+//}
+
+
+// var string = "sarapan"
+// var temp = ''
+
+// for (var i = 0; i < string.length; i++) {
+//     if (i % 2 === 0) {
+//         temp = temp + string[i].toUpperCase()
+//     }
+//     else {
+//         temp = temp + string[i]
+//         // console.log(string[i])
+//     }
+// }
+
+// console.log(temp)
+
+// var nomor = 3
+// var temp = ""
+
+// for (var i = 0; i < nomor; i++) {
+//     for (var j = 0; j < nomor; j++) {
+//         temp += "#"
+//     }
+//     console.log(temp)
+//     temp = ""
+// }
+
+
+var string = ""
+var number = 5;
+
+// for (var i = 1; i <= number; i++) {
+//     //MIRING KE KIRI
+//     for (var j = 0; j <= number - i; j++){
+//         string += " "
+//     }
+//     for (var k = 1; k <= i; k++) {
+//         string += k
+//     }
+//     string += "\n"
+
+    //MIRING KE KANAN
+    // for (var l = 1; l <= i; l++){
+    //     string += l
+    // }
+    // string += "\n"
+//}
+
+// for (var i = 1; i <= number; i++){
+//     for (var j = number; j >= i; j--) {
+//         string += j;
+//         //string += j - 1;
+//     }
+//     console.log(string);
+//     string = ""
+// }
+// console.log(string)
+
+
+
+
+// for (var i = 0; i < number; i++) {
+//     for (var j = 0; j <= i; j++) {
+//         string += (j + 1)
+//     }
+//     console.log(string)
+//     string = ""
+// }
+
+
+
+
+
